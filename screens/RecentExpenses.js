4e6aaaacfd5b6b4e785e0a0789d01e5a4da5a762
Salvatore Castellitti <s.castellitti@gmail.com>
2023-03-16 19:58:35 +0100

    Added components and style

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,7 +1,8 @@
 import { Text } from "react-native";
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 function RecentExpenses() {
-  return <Text>Recent Expenses</Text>;
+  return <ExpensesOutput expensesPeriod="7 Days" />;
 }
 
 export default RecentExpenses;
