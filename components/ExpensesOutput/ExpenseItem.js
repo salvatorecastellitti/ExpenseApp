80d57c4f4f5f16619b4a5fb561dc1ae0076b550d
Salvatore Castellitti <s.castellitti@gmail.com>
2023-03-18 17:02:54 +0100

    route for manage expenses

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,10 +1,18 @@
 import { Pressable, View, StyleSheet, Text } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../utils/date";
+import { useNavigation } from "@react-navigation/native";
 
-function ExpenseItem({ description, amount, date }) {
+function ExpenseItem({ id, description, amount, date }) {
+  const navigation = useNavigation();
+  function expensePressHandler() {
+    navigation.navigate("ManageExpenses", { expenseId: id });
+  }
   return (
-    <Pressable>
+    <Pressable
+      onPress={expensePressHandler}
+      style={({ pressed }) => pressed && styles.pressed}
+    >
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textbase, styles.description]}>
@@ -56,5 +64,8 @@ const styles = StyleSheet.create({
   amount: {
     color: GlobalStyles.colors.primary500,
     fontWeight: "bold",
+  },
+  pressed: {
+    opacity: 0.75,
   },
 });
---
+++ components/UI/IconButton.js
@@ -0,0 +1,29 @@
+import { Pressable, View, StyleSheet } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+function IconButton({ icon, color, size, onPress }) {
+  return (
+    <Pressable
+      style={({ pressed }) => pressed && styles.pressed}
+      onPress={onPress}
+    >
+      <View style={styles.buttonContainer}>
+        <Ionicons name={icon} color={color} size={size} />
+      </View>
+    </Pressable>
+  );
+}
+
+export default IconButton;
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 24,
+    padding: 6,
+    marginHorizontal: 8,
+    marginVertical: 2,
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
